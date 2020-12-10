27ac2b7357f61e19e4860c21517fde2ccf90bbce
smartli <smartli_it@163.com>
2020-12-10 09:44:27 +0800

    hello world 案例实现

--- hellowworld/views.py
+++ hellowworld/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def helloworld_view(request):
+    return HttpResponse('<h1>hello world</h1>')
