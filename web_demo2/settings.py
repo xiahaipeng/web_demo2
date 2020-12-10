a243705b50622447a7a2fc8459a44a83dd0b5181
smartli <smartli_it@163.com>
2020-12-10 10:06:48 +0800

    注册案例--普通实现

--- web_demo2/settings.py
+++ web_demo2/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hellowworld.apps.HellowworldConfig'
+    'hellowworld.apps.HellowworldConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'web_demo2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
